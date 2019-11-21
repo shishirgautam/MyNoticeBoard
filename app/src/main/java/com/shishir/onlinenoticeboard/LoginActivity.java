e6aea76b560653978e677f0cf52997c046631247
shishirgautam <shishirgautam2gmail.com>
2019-11-21 12:53:58 +0545

    Login page Design

---
+++ app/src/main/java/com/shishir/onlinenoticeboard/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.shishir.onlinenoticeboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText Email,Password;
+    private Button Login;
+
+    String user,pwd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Email = findViewById(R.id.etemail);
+        Password = findViewById(R.id.etpassword);
+
+        Login = findViewById(R.id.btn_login);
+
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = Email.getText().toString().trim();
+                pwd = Password.getText().toString().trim();
+
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = Email.getText().toString().trim();
+                pwd = Password.getText().toString().trim();
+//                if (user.equals("ad")&& pwd.equals("ad")) {
+//                        Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+//                        startActivity(intent);
+//
+//                    }
+
+
+                if (!user.isEmpty()&& !pwd.isEmpty()){
+                    if (user.equals("admin")&& pwd.equals("admin")) {
+                        Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                        startActivity(intent);
+
+                    }
+                }
+
+                else{
+                    if (user.isEmpty()){
+                        Email.setError("Enter User Name");
+                    }
+                    if (pwd.isEmpty()){
+                        Email.setError("Enter Password ");
+                    }
+                }
+            }
+
+        });
+
+
+    }
+    }
+
