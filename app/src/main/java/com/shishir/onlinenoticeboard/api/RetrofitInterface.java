2160e34e5e3950166cb8f62edcba8e6bb36903a0
shishirgautam <gautamshishir2@gmail.com>
2020-01-30 20:01:51 +0545

    Updated Api functionality with strictMode

--- app/src/main/java/com/shishir/onlinenoticeboard/api/RetrofitInterface.java
+++ app/src/main/java/com/shishir/onlinenoticeboard/api/RetrofitInterface.java
@@ -6,10 +6,12 @@ import com.shishir.onlinenoticeboard.model.LoginResult;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface RetrofitInterface {
     @POST("/users/login")
+    @FormUrlEncoded
     Call<LoginResult>executeLogin(@Field("email") String username, @Field("password") String password);
 
     @POST("/users/register")
